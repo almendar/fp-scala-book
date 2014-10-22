56b1bcb9897a9d765d0c773a1bf518feae9554e5
Tomasz Kogut <tomasz.kogut@attr.com.pl>
2014-10-22 15:10:08 +0200

    Started Chapter 7 on parallelism

---
+++ src/main/scala/fpinscala/functionalparallelism/Ch7Future.scala
@@ -0,0 +1,41 @@
+package fpinscala.functionalparallelism
+
+import scala.collection.immutable.IndexedSeq
+import java.util.concurrent.{Callable,TimeUnit}
+
+import Par.Par
+
+object Util {
+  def sum(as : IndexedSeq[Int]) : Par[Int] =
+  	if (as.size <= 1) Par.unit(as.headOption getOrElse 0)
+	else {
+		val (l,r) = as.splitAt(as.length/2)
+		Par.map2(Par.fork(sum(l)), Par.fork(sum(r)))(_ + _)
+	}
+}
+
+
+class ExecutorService {
+	def submit[A](a: Callable[A]): Future[A] = ???
+}
+
+trait Future[A] {
+	def get: A
+	def get(timeout: Long, unit: TimeUnit): A
+	def cancel(evenIfRunning: Boolean): Boolean
+	def isDone: Boolean
+	def isCancelled: Boolean
+}
+
+object Par {
+
+  type Par[A] = ExecutorService => Future[A]
+
+  def unit[A](a : => A) : Par[A] = ???
+  def map2[A,B,C](p1 : Par[A], p2 : Par[B])(f : (A,B) => C) : Par[C] = ???
+  def fork[A](a: => Par[A]) : Par[A] = ???
+  def async[A](a : => A) : Par[A] = fork(unit(a))
+  def run[A](s: ExecutorService)(a : Par[A]) : A = ???
+}
+
+
