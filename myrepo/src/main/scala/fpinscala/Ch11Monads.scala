3463dc06d5554c91ecc26676bb40d0b3a60c72f4
Tomasz Kogut <tomasz.kogut@hotmail.com>
2014-12-06 23:36:38 +0100

    Added a few combinators to Monad trait

--- myrepo/src/main/scala/fpinscala/Ch11Monads.scala
+++ myrepo/src/main/scala/fpinscala/Ch11Monads.scala
@@ -15,8 +15,28 @@ trait Monad[M[_]] extends Functor[M] {
 	def map2[A,B,C](ma: M[A], mb: M[B])(f: (A, B) => C): M[C] = 
 		flatMap(ma)(a => map(mb)(b => f(a,b)))
 
+	def sequence[A](lma : List[M[A]]) : M[List[A]] = 
+		lma.foldLeft(unit(List()) : M[List[A]])((acc,el) => map2(el,acc)(_ :: _))
+	def traverse[A,B](la : List[A])(f: A => M[B]) : M[List[B]] = 
+		la.foldLeft(unit(List()) : M[List[B]])((acc,el) => map2(f(el),acc)(_ :: _))
+
+	def _replicateM[A](n: Int, ma: M[A]): M[List[A]] = 
+		if (n <= 0) unit(List[A]()) else map2(ma, replicateM(n - 1, ma))(_ :: _)
+
+	def replicateM[A](n: Int, ma: M[A]): M[List[A]] = sequence(List.fill(n)(ma))
+
 	def compose[A,B,C](f: A => M[B] , g: B => M[C]) : A => M[C] = (a:A) => 
 		flatMap(f(a))(g)
+
+
+	def factor[A,B](ma: M[A], mb: M[B]): M[(A, B)] = map2(ma, mb)((_, _))
+
+	def cofactor[A,B](e: Either[M[A], M[B]]): M[Either[A, B]] = e match {
+		case Left(ma) => map(ma)(x => Left(x))
+		case Right(mb) => map(mb)(y => Right(y))
+	}
+
+	def join[A](mma: M[M[A]]): M[A] = flatMap(mma)(ma => ma)
 
 }
 
@@ -30,6 +50,11 @@ object Ch11Monads extends App {
 		def flatMap[A,B](la : List[A])(f: A => List[B]) : List[B] = la flatMap f
 	} 
 
+
+	val p : Either[List[String], List[Int]] = Right(List(1,2,3,4))
+
+
+	println(listMonad.cofactor(p))
 
 
 	println(listMonad.compose((x:Int) => List.fill(x)(x), (y:Int) => List.fill(y)(y))(2))
